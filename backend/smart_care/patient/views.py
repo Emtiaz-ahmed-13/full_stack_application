cb6fa737defc698a89b8b962c47df3631b5808b4
EMTIAZAHMED2060 <emtiaz.ahmed@g.bracu.ac.bd>
2024-04-01 03:46:14 +0600

    till send email

--- backend/smart_care/patient/views.py
+++ backend/smart_care/patient/views.py
@@ -4,8 +4,36 @@ from rest_framework import viewsets
 # Create your views here.
 from . import models
 from . import serializers
+from rest_framework.views import APIView
+from django.contrib.auth.tokens import default_token_generator
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.core.mail import EmailMultiAlternatives
+from rest_framework.response import Response
 
 class PatientViewset(viewsets.ModelViewSet):
     queryset = models.Patient.objects.all()
     serializer_class = serializers.PatientSerializer
-    
+
+class UserRegistrationApiView(APIView):
+    serializer_class = serializers.RegistrationSerializer
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        
+        if serializer.is_valid():
+            user = serializer.save()
+            print(user)
+            token = default_token_generator.make_token(user)
+            print("token ", token)
+            uid = urlsafe_base64_encode(force_bytes(user.pk))
+            print("uid ", uid)
+            confirm_link = f"https://smart-care.onrender.com/patient/active/{uid}/{token}"
+            email_subject = "Confirm Your Email"
+            email_body = render_to_string('confirm_email.html', {'confirm_link' : confirm_link})
+            
+            email = EmailMultiAlternatives(email_subject , '', to=[user.email])
+            email.attach_alternative(email_body, "text/html")
+            email.send()
+            return Response("Check your mail for confirmation")
+        return Response(serializer.errors)
